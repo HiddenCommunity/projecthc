792fb52fb95cc7975b0c3473b3167843076e1cf4
chaeunbyeol <ceb0527@ajou.ac.kr>
2016-11-12 20:13:12 +0900

    member create(complete) / member retrieve................................

--- routes/members.js
+++ routes/members.js
@@ -26,7 +26,7 @@ router.route('/')
                 return console.error(err);
             } else {
                 //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
-                //res.format({
+                res.format({
                 //HTML response will render the index.jade file in the views/members folder. We are also setting "members" to be an accessible variable in our jade view
                 //html: function(){
                 //res.render('members/index', {
@@ -35,12 +35,13 @@ router.route('/')
                 //    });
                 //   },
                 //JSON response will show all members in JSON format
-                //    json: function(){
-                res.json(members);
-                //}
+                    json: function(){
+                        res.json(members);
+                    }
+                });
             }
         });
-    }
+    })
 
     //POST 회원 추가 (회원가입 form 에서 데이터가 넘어옴)
     .post(function(req, res) {
@@ -59,12 +60,12 @@ router.route('/')
                 console.log('POST creating new member: ' + member);
                 res.format({
                     //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
-                    html: function(){
+                    //html: function(){
                         // If it worked, set the header so the address bar doesn't still say /adduser
-                        res.location("members");
+                    //    res.location("members");
                         // And forward to success page
-                        res.redirect("/members");
-                    },
+                    //    res.redirect("/members");
+                    //},
                     //JSON response will show the newly created member
                     json: function(){
                         res.json(member);
@@ -73,7 +74,7 @@ router.route('/')
             }
         })
     })
-    )
+
     // 회원가입 페이지
     router.get('/new', function(req, res) {
         //렌더링할 라우트. /new 요청을 하면, view/members/new.jade 파일이 HTML형식으로 렌더링된다.
