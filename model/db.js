98984db6d1810e5bcfaf601891269f0d92106435
DESKTOP-MINSEOK\Minseok <glaxer95@gmail.com>
2016-11-13 14:48:35 +0900

    local 개발환경 설정.

--- model/db.js
+++ model/db.js
@@ -1,3 +1,28 @@
 //Setup Mongoose to MongoDB
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/hcDB');
+if (process.platform == "win32") {
+    console.log(`this platform is ${process.platform}`)
+    var fs = require('fs');
+    var tunnel = require('tunnel-ssh');
+
+    var key = fs.readFileSync('../projecthc/hc_server.pem');
+    var config = {
+        port: 22,
+        username: 'centos',
+        host: '52.78.207.133',
+        dstPort:'27017',
+        privateKey:key
+    };
+
+    var hc_db = tunnel(config,function(error,hc_db){
+        if(error){
+            console.log("SSH connection error: " + error);
+        }
+        console.log('ssh connection initalizing');
+        mongoose.connect('mongodb://localhost:27017/hcDB');
+        console.log('data base connect complete');
+    });
+} else {
+    mongoose.connect('mongodb://localhost:27017/hcDB');
+}
+
