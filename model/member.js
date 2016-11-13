65390723db8b75edff73444a8ec6291931d58f31
chaeunbyeol <ceb0527@ajou.ac.kr>
2016-11-13 18:58:04 +0900

    images 폴더 추가, member.js 스키마 변경

--- model/member.js
+++ model/member.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var memberSchema = new Schema({
+    email: String,
     nickname: String,
     connectKey: String,
     majors: [{category: String}],
