46d4488cfea05ed23b7836d6812dcbe6f629238f
chaeunbyeol <ceb0527@ajou.ac.kr>
2016-11-13 18:12:38 +0900

    이메일 보냈을 때, post로 변경

--- routes/index.js
+++ routes/index.js
@@ -409,7 +409,7 @@ router.route('/boards/:id/edit')
 
 //메일보내기
 router.route('/send/email/:email')
-    .get(function(req, res){
+    .post(function(req, res){
       var email = req.params.email;
       console.log(email);
       // create reusable transporter object using the default SMTP transport
