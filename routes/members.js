95357e6c91450fb7ae9ff99240f1f1e10e32c8aa
unknown <>
2016-11-23 19:27:28 +0900

    req.param('')쓰지말래서 req.params 로 바꿔봄..

--- routes/members.js
+++ routes/members.js
@@ -50,11 +50,11 @@ route.route('/addInfo')
     })
     .post(function (req,res){
         console.log("들어왔음");
-        var email = req.param('email');
-        var nickname = req.param('nickname');
-        var major1 = req.param('major1');
-        var major2 = req.param('major2');
-        var major3 = req.param('major3');
+        var email = req.params.email;
+        var nickname = req.params.nickname;
+        var major1 = req.params.major1;
+        var major2 = req.params.major2;
+        var major3 = req.params.major3;
         console.log(email, nickname, major1);
         //이메일을 찾아서
         mongoose.model('Member').findOne({'email': email }, function (err, member) {
