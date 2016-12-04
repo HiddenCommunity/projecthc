949497c353c048f93315165d194e9cf88d50244e
unknown <>
2016-12-04 21:03:03 +0900

    MESSAGE 테스트

--- routes/messages.js
+++ routes/messages.js
@@ -4,10 +4,10 @@ var express = require('express'),
 
 //메시지 전송
 route.route('/send/:recipient')
+    // .get(function (req, res) {
+    //     res.render('messages/send', {title: '메시지 전송'});
+    // })
     .get(function (req, res) {
-        res.render('messages/send', {title: '메시지 전송'});
-    })
-    .post(function (req, res) {
         // 웹테스트용
         // var recipient = req.body.recipient;
         // var sender = req.body.sender;
@@ -25,11 +25,11 @@ route.route('/send/:recipient')
         }, function (err, msg) {
             if (err) {
                 res.send("[error] 메시지 전송 실패");
-                console.log('POST [실패] 메시지 전송 실패');
+                console.log('GET [실패] 메시지 전송 실패');
             } else { //메시지 전송 성공
                 //res.json({message : msg.body});
                 res.redirect('http://52.78.207.133:3000/messages/list/'+ recipient);
-                console.log('POST [성공] 메시지 전송 성공 ' + msg._id);
+                console.log('GET [성공] 메시지 전송 성공 ' + msg._id);
             }
         });
     });
