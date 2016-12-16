2b8e061422d93c08540fbdd3eb2a96f0c084df0d
unknown <>
2016-12-16 19:04:43 +0900

    닉네임 테스트중

--- routes/members.js
+++ routes/members.js
@@ -53,6 +53,9 @@ route.post('/checkNickname/:nickname', function(req, res) {
         if (err) {
             return console.error(err);
         } else {
+            console.log('member.nickname : ' + member.nickname);
+            console.log('nickname : '+ nickname);
+            console.log('member : '+member);
             if(member.nickname == nickname){
                 console.log('이미 존재하는 닉네임입니다.');
                 res.json({response:"no"});
